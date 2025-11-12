a404d7bde209f0bad771c99e61f2388c3eac0c04
eyadadel16007389 <Eyad.Elmoataz1234567@gmail.com>
2025-11-12 15:11:28 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,8 +4,24 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal {
+private :
+    string name;
+    int age;
+    bool isHungry;
+public :
+    Animal();
+    Animal(string name , int age , bool isHungry);
+    void display();
+    void feed();
+    void set_name(string name);
+    void set_age(int age);
+    string get_name();
+    int get_age();
+class Mammal :: class Animal {string furcolour};
+class Bird ::  class Animal {float wingSpan};
+class Reptile :: public Animal{bool isVenomous};
 
 
-
-#endif
+};
+#endif // ANIMAL_H_INCLUDED
