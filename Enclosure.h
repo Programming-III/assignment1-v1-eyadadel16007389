bc63abea22a340efe2aed89e77a94da32aa2e9d1
eyadadel16007389 <Eyad.Elmoataz1234567@gmail.com>
2025-11-12 15:12:55 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,21 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+
+class Enclosure {
+private :
+    Animal* animal;
+    int capacity;
+    int currentCount;
+public :
+    Enclosure();
+    Enclosure(Animal* animal , int capacity , int currentCount);
+    void set_capacity(int ca);
+    int get_capacity();
+    void set_current(int cu);
+    int get_current();
+    void addAnimal(Animal* a);
+    void displayAnimals();
 
 
 
